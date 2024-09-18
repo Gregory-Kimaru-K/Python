28426b9eec7727adcc8e54b66bfc8eeca73f1ce3
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-09-18 10:41:08 +0300

    Fibbonacci

--- Basic/4.py
+++ Basic/4.py
@@ -1,1 +1,13 @@
 def fibonnacci(num):
+    fib_list = [0, 1]
+
+    i = 2
+    while i <= num:
+        fib_list.append(fib_list[i-1] + fib_list[i-2])
+        i += 1
+    
+    return fib_list
+
+fibo = fibonnacci(10)
+
+print(fibo)

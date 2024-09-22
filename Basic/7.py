725d3d3accc0712dab4eac794f5f1276dc97daa2
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-09-22 09:31:19 +0300

    Prime Numbers

--- Basic/7.py
+++ Basic/7.py
@@ -1,2 +1,26 @@
-def IsPrime():
-    
+import math
+def IsPrime(num):
+    if num < 0:
+        num = num*-1
+
+    if num <= 1:
+        print(f'Try number greater than 1')
+        return
+
+    i = 2
+
+    while i < math.sqrt(num):
+        if num % i == 0:
+            return False
+        i += 1
+
+    return True
+
+number = int(input("Enter a number: ")) 
+checkprime = IsPrime(number)
+
+if checkprime == True:
+    print(f'{number} is a prime number')
+
+else:
+    print(f"{number} is not a prime number")
